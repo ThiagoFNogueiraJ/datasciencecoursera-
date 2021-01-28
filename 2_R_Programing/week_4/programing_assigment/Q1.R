ceca17f63fff4e42a523268030471b848572e34f
Thiago Felipe Nogueira <thiago.nogueira@defesacivil.pms.ad>
2021-01-28 14:55:57 -0300

    Script for the first step of programing assigment

--- 2_R_Programing/week_4/programing_assigment/Q1.R
+++ 2_R_Programing/week_4/programing_assigment/Q1.R
@@ -15,12 +15,13 @@ outcome[all_four,2]
 outcome[,7]
 
 
-pdata <- read.csv("week_4/programing_assigment/data/outcome-of-care-measures.csv", sep = ",")
-state <- "TX"
-num  <- 4
+pdata <- read.csv("2_R_Programing/week_4/programing_assigment/data/outcome-of-care-measures.csv", sep = ",")
+state <- "MN"
+num  <- 5000
 outcome <- "heart failure"
 sdata<- subset(pdata, State == state, select = c(2,11, 17,23))
 dim(sdata)
+if (is.numeric(num) & num > length(unique(sdata$Hospital.Name))) {stop(NA)}
 if (outcome == "heart attack"){
   sdata[,2] <- as.numeric(sdata[,2])
   if (num == "best") {
@@ -33,7 +34,7 @@ if (outcome == "heart attack"){
     all_best <- which(sdata[,2] == sort(sdata[,2], na.last =TRUE)[num])
   }
   best_h <- sdata[all_best, 1]
-} 
+} else  print("não é heart attack")   
 if (outcome == "heart failure"){
   sdata[,3] <- as.numeric(sdata[,3])
   if (num == "best") {
@@ -43,10 +44,10 @@ if (outcome == "heart failure"){
     all_best <- which(sdata[,3] == max(sdata[,3], na.rm = TRUE))
   } 
   if (is.integer(num) == TRUE) {
-    all_best <- which(sdata[,3] == sort(sdata[,3], na.last =TRUE)[num])
+    all_best <- which(sdata[,3] == order(sdata, c(3,2))[num])
   }
   best_h <- sdata[all_best, 1]
-} 
+} else  print("não é heart failure")   
 if (outcome == "pneumonia"){
   sdata[,4] <- as.numeric(sdata[,4])
   if (num == "best") {
@@ -62,3 +63,4 @@ if (outcome == "pneumonia"){
 } 
 best_h
 
+s
